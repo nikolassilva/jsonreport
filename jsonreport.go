8c6c5e010edf9a67b1cac74aa7c0c3fab018d5bb
Nikolas Silva <nikolas.rsilva@gmail.com>
2017-05-30 02:00:51 -0300

    :sparkles: Add WriteXLSX method

--- jsonreport.go
+++ jsonreport.go
@@ -3,6 +3,9 @@ package jsonreport
 import (
 	"encoding/csv"
 	"io"
+	"strconv"
+
+	"github.com/Luxurioust/excelize"
 )
 
 // Report TODO
@@ -14,8 +17,9 @@ type Report struct {
 
 // Options TODO
 type Options struct {
-	Comma   rune
-	UseCRLF bool
+	Comma     rune
+	UseCRLF   bool
+	SheetName string
 }
 
 // WriteCSV TODO
@@ -33,4 +37,39 @@ func (report *Report) WriteCSV(w io.Writer) error {
 	}
 
 	return writer.WriteAll(report.Records)
+}
+
+// WriteXLSX TODO
+func (report *Report) WriteXLSX(w io.Writer) error {
+	xlsx := excelize.CreateFile()
+	sheetName := xlsx.GetSheetName(xlsx.GetActiveSheetIndex())
+
+	setExcelRow(xlsx, sheetName, report.Headers, 0)
+	for i, row := range report.Records {
+		setExcelRow(xlsx, sheetName, row, i+1)
+	}
+
+	if report.Options.SheetName != "" {
+		xlsx.SetSheetName(sheetName, report.Options.SheetName)
+	}
+
+	return xlsx.Write(w)
+}
+
+func setExcelRow(xlsx *excelize.File, sheetName string, columns []string, rowIndex int) {
+	for i, column := range columns {
+		cell := colStr(i+1) + strconv.Itoa(rowIndex+1)
+		xlsx.SetCellStr(sheetName, cell, column)
+	}
+}
+
+func colStr(i int) string {
+	var str string
+	for i > 0 {
+		rest := (i - 1) % 26
+		str = string(rest+65) + str
+		i = (i - rest) / 26
+	}
+
+	return str
 }
